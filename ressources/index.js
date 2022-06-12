3dfae7cb71a0c0bca4eb66d1a8bcfb8b854ce974
leliev <lelievrevincent@free.fr>
2022-06-12 22:40:40 +0200

    NavBar, Sub slides footer and fontawesome

--- ressources/index.js
+++ ressources/index.js
@@ -19,17 +19,24 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
+  let subSlides = document.getElementsByClassName("sub-slides");
   let dots = document.getElementsByClassName("dot");
+  let navLinks = document.getElementsByClassName("nav-link");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    subSlides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
+    navLinks[i].className = navLinks[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+  subSlides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  navLinks[slideIndex-1].className += " active";
 }
 
 // Stat bars creation(html fragment might be faster)
